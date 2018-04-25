190b8ae0c145de41296f290b423e544fe31782d8
unknown <KEWEN@huazhu.com>
2018-04-25 20:43:57 +0800

    'lesson-4'

---
+++ src/components/pages/ShoppingMall.vue
@@ -0,0 +1,40 @@
+<!--  -->
+<template>
+  <div>
+      <van-row>
+          <van-col span='8'>span:8</van-col>
+          <van-col span='8'>span:8</van-col>
+          <van-col span='8'>span:8</van-col>
+      </van-row>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [],
+
+  data () {
+    return {
+        msg: 'Hello'
+    };
+  },
+
+  created(){},
+
+  mounted(){},
+
+  watch: {},
+
+  computed: {},
+
+  methods: {},
+
+  components: {}
+}
+
+</script>
+<style scoped>
+    .test-row{
+        text-align: center;
+    }
+</style>
