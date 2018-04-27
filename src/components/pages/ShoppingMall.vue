ff1b4c727af27f3ddf9f82384a527988c6964dec
unknown <KEWEN@huazhu.com>
2018-04-27 16:08:22 +0800

    'lesson5'

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -1,10 +1,16 @@
 <!--  -->
 <template>
-  <div>
+  <div class="search-bar">
       <van-row>
-          <van-col span='8'>span:8</van-col>
-          <van-col span='8'>span:8</van-col>
-          <van-col span='8'>span:8</van-col>
+          <van-col span="3">
+              <img :src="locationIcon" width="80%" class="location-icon" />
+          </van-col>
+          <van-col span="16">
+              <input type="text" class="search-input">
+          </van-col>
+          <van-col span="5">
+              <van-button size="mini">查找</van-button>
+          </van-col>
       </van-row>
   </div>
 </template>
@@ -15,7 +21,8 @@ export default {
 
   data () {
     return {
-        msg: 'Hello'
+        msg: 'Hello',
+        locationIcon: require('../../assets/logo.png')
     };
   },
 
@@ -34,7 +41,20 @@ export default {
 
 </script>
 <style scoped>
-    .test-row{
-        text-align: center;
+    .search-bar{
+        height: 2.2rem;
+        background-color: #e5017d;
+        line-height: 2.2rem;
+    }
+    .search-input{
+        width: 100%;
+        height: 1.3rem;
+        border: 0 0 1px 0;
+        border-bottom: 1px solid #fff;
+        background-color: #e5017d;
+        color: #fff;
+    }
+    .location-icon{
+        padding: .2rem 0 0 .3rem;
     }
 </style>
